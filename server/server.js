8c67c22e9d06fc16998fbc0e72d5bdfb5711352f
komalraj-dev <komalraj0207@gmail.com>
2026-08-03 20:48:50 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ connectDB();
 const app = express();
 
 // Core middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -35,6 +40,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
